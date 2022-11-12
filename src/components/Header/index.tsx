58b02b40e9a1706f7505d138cb12b9c06a880f06
Moisés Neto <moisesnetored@gmail.com>
2022-11-12 16:49:58 -0400

    feat: header and layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt="Logo da aplicação que tem  forma de dois triangulos"
+      />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
